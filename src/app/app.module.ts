fec7aa5b26c6ff96c05653425c4df550a5e836ab
Fred Mandrea <fred.mandrea@gmail.com>
2019-03-21 17:23:14 +0100

    Formulaire de modification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { UserDetailsComponent } from './user-details/user-details.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { User } from '../models/user';
+import { FormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-details',
@@ -11,33 +13,68 @@ import { User } from '../models/user';
 export class UserDetailsComponent implements OnInit {
 
   user: User;
-
   error: string;
+  private _routeSub: Subscription;
+  formulaire;
 
   constructor(
     private _activatedRoute: ActivatedRoute,
-    private _httpClient: HttpClient
+    private _httpClient: HttpClient,
+    private _router: Router,
+    private _fb: FormBuilder
     ) {
-      this._activatedRoute.params.subscribe(
+      this._routeSub = this._activatedRoute.params.subscribe(
         (params) => {
-          this.getCurrentUser(params.userid);
+          this._getCurrentUser(params.userid);
         }
       );
 
     };
 
-  getCurrentUser(userid: number) {
+  private _initForm() {
+    this.formulaire = this._fb.group({
+      firstname: this._fb.control(this.user.firstname ),
+      lastname: this._fb.control(this.user.lastname ),
+      email: this._fb.control(this.user.email ),
+      avatar: this._fb.control(this.user.avatar )
+    });
+  }
+
+  private _getCurrentUser(userid: number) {
     this._httpClient.get<User>('http://localhost:3000/users/' + userid)
     .subscribe(
       resultat => {
         this.user = resultat;
+        this._initForm();
       },
       (err: HttpErrorResponse) => {
         this.error = err.message;
       }
+    )
+  }
+
+  modifieUtilisateur() {
+    this.user.firstname = this.formulaire.get('firstname').value;
+    this.user.lastname = this.formulaire.get('lastname').value;
+    this.user.email = this.formulaire.get('email').value;
+    this.user.avatar = this.formulaire.get('avatar').value;
+
+    this._httpClient.put<User>('http://localhost:3000/users/' + this.user.id, this.user)
+    .subscribe(
+      resultat => {
+        this._router.navigateByUrl('/users');
+      },
+      (err: HttpErrorResponse) => {
+        this.error = err.message;
+      }
+    )
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this._routeSub.unsubscribe();
   }
 
 }
